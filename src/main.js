e25cf780d5efa9b8460755dc2ddbd778175a91a5
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2025-06-24 07:25:44 +0800

    WIP: ui

--- src/main.js
+++ src/main.js
@@ -1,34 +1,70 @@
 
 import "./styles.css";
 
+const numberOfCells = 64;
+const cellContainer = document.querySelector(".cell-container");
+const body = document.querySelector("body");
+const dragData = {
+    isThereAnElementGettingDragged: false,
+    current: null,
+    distanceFromMiddlePoint: null
+}
 
-const box = document.querySelector(".box");
-const boxContainer = document.querySelector(".box-container");
+// const shipSizes = [1, 1, 2, 2, 3, 3, 4];
 
-let temp = null;
-let tempFilled = false;
 
-box.addEventListener("mousedown", e => {
-    temp = e.target;
-    tempFilled = true;
-})
+function createCells(parentElement){
 
-box.addEventListener("mouseup", e => {
-    temp = null;
-    tempFilled = false;
-})
+    for(let i = 0; i < numberOfCells; i++){
 
-boxContainer.addEventListener("mouseover", e => {
-    if(tempFilled){
-        boxContainer.appendChild(temp);
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        parentElement.appendChild(cell);
     }
-})
+}
 
-boxContainer.addEventListener("mouseout", e => {
-    if(tempFilled){
-        boxContainer.appendChild(temp);
+function createShip(shipNumber, shipLength){
+    const ship = document.createElement("div");
+    ship.classList.add("ship");
+    ship.addEventListener("mousedown", () => {
+        dragData.isThereAnElementGettingDragged = true;
+        dragData.current = shipNumber;
+        console.log(dragData);
+    });
+    ship.addEventListener("mouseup", () => {
+        dragData.isThereAnElementGettingDragged = false;
+        dragData.current = null;
+        console.log(dragData);
+
+    });
+
+    let distanceFromMiddlePoint = Math.trunc(shipLength / 2); 
+    for(let i = 0; i < shipLength; i++){
+        const shipSection = document.createElement("div");
+        shipSection.classList.add("ship-section");
+        bindEventListenerToShipSection(shipSection, distanceFromMiddlePoint);
+        distanceFromMiddlePoint -= 1;
+        console.log(shipSection);
+        ship.appendChild(shipSection);
     }
-})
+
+
+    body.appendChild(ship);
+}
+
+function bindEventListenerToShipSection(shipSection, distanceFromMiddlePoint){
+    shipSection.addEventListener("mousedown", () => {
+        dragData.distanceFromMiddlePoint = distanceFromMiddlePoint; 
+    });
+
+    shipSection.addEventListener("mouseup", () => {
+        dragData.distanceFromMiddlePoint = null; 
+    });
+}
+
+createShip(1, 4);
+createCells(cellContainer); 
+
 
 
 
