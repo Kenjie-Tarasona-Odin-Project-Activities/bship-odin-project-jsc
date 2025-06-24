201f21480944bcc0e714a0d1757447cefc94f916
Kenjie Tarasona <148972543+Lain-lies@users.noreply.github.com>
2025-06-24 18:13:02 +0800

    WIP: ui continuation

--- src/main.js
+++ src/main.js
@@ -1,71 +1,89 @@
-
 import "./styles.css";
 
 const numberOfCells = 64;
 const cellContainer = document.querySelector(".cell-container");
 const body = document.querySelector("body");
 const dragData = {
-    isThereAnElementGettingDragged: false,
-    current: null,
-    distanceFromMiddlePoint: null
-}
+  isThereAnElementGettingDragged: false,
+  current: null,
+  distanceFromMiddlePoint: null,
+  startX: null,
+  startY: null,
+};
 
 // const shipSizes = [1, 1, 2, 2, 3, 3, 4];
 
-
-function createCells(parentElement){
-
-    for(let i = 0; i < numberOfCells; i++){
-
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        parentElement.appendChild(cell);
-    }
+function createCells(parentElement) {
+  for (let i = 0; i < numberOfCells; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    parentElement.appendChild(cell);
+  }
 }
 
-function createShip(shipNumber, shipLength){
-    const ship = document.createElement("div");
-    ship.classList.add("ship");
-    ship.addEventListener("mousedown", () => {
-        dragData.isThereAnElementGettingDragged = true;
-        dragData.current = shipNumber;
-        console.log(dragData);
-    });
-    ship.addEventListener("mouseup", () => {
-        dragData.isThereAnElementGettingDragged = false;
-        dragData.current = null;
-        console.log(dragData);
+function createShip(shipNumber, shipLength) {
+  const ship = document.createElement("div");
+  ship.classList.add("ship");
 
-    });
+  bindEventListenerToShip(ship, shipNumber);
 
-    let distanceFromMiddlePoint = Math.trunc(shipLength / 2); 
-    for(let i = 0; i < shipLength; i++){
-        const shipSection = document.createElement("div");
-        shipSection.classList.add("ship-section");
-        bindEventListenerToShipSection(shipSection, distanceFromMiddlePoint);
-        distanceFromMiddlePoint -= 1;
-        console.log(shipSection);
-        ship.appendChild(shipSection);
-    }
+  let distanceFromMiddlePoint = Math.trunc(shipLength / 2);
+  for (let i = 0; i < shipLength; i++) {
+    const shipSection = document.createElement("div");
+    shipSection.classList.add("ship-section");
 
+    bindEventListenerToShipSection(shipSection, distanceFromMiddlePoint);
+    distanceFromMiddlePoint -= 1;
 
-    body.appendChild(ship);
+    ship.appendChild(shipSection);
+    console.log(shipSection);
+  }
+
+  body.appendChild(ship);
 }
 
-function bindEventListenerToShipSection(shipSection, distanceFromMiddlePoint){
-    shipSection.addEventListener("mousedown", () => {
-        dragData.distanceFromMiddlePoint = distanceFromMiddlePoint; 
-    });
+function bindEventListenerToShipSection(shipSection, distanceFromMiddlePoint) {
+  shipSection.addEventListener("mousedown", () => {
+    dragData.distanceFromMiddlePoint = distanceFromMiddlePoint;
+  });
 
-    shipSection.addEventListener("mouseup", () => {
-        dragData.distanceFromMiddlePoint = null; 
-    });
+  shipSection.addEventListener("mouseup", () => {
+    dragData.distanceFromMiddlePoint = null;
+  });
+}
+
+function bindEventListenerToShip(ship, shipNumber) {
+  ship.addEventListener("mousedown", (e) => {
+    dragData.isThereAnElementGettingDragged = true;
+    dragData.current = shipNumber;
+    dragData.startX = e.clientX;
+    dragData.startY = e.clientY;
+
+    console.log(dragData);
+  });
+
+  ship.addEventListener("mouseup", () => {
+    dragData.isThereAnElementGettingDragged = false;
+    dragData.current = null;
+    dragData.startX = null;
+    dragData.startY = null;
+
+    console.log(dragData);
+  });
+
+  ship.addEventListener("mousemove", (e) => {
+    if (dragData.isThereAnElementGettingDragged) {
+      let newX = dragData.startX - e.clientX;
+      let newY = dragData.startY - e.clientY;
+
+      dragData.startX = e.clientX;
+      dragData.startY = e.clientY;
+
+      ship.style.left = `${ship.offsetLeft - newX}px`;
+      ship.style.top = `${ship.offsetTop - newY}px`;
+    }
+  });
 }
 
 createShip(1, 4);
-createCells(cellContainer); 
-
-
-
-
-
+createCells(cellContainer);
